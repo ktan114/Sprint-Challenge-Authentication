eb44c7cf08858eb24b239784e25d795cd7d5f1c4
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-18 13:45:59 -0400

    Adding route and Sigin component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route, withRouter } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+
+  // componentDidMount() {
+  //   axios.get(`http://localhost:5000/api/jokes`)
+  //   .then(res => {
+  //     this.setState({ jokes: res.data })
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Route path = '/signin' component={Signin} />
+
       </div>
     );
   }
--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const corsOptions = {
   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
-  origin: 'http://localhost:5000',
-  credentials: true
+  origin: 'http://localhost:3000',
+  credentials: true,
+  'Access-Control-Allow-Methods': 'GET, POST'
 };
 
 server.use(express.json());
