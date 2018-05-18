fcc7e5de1373df90d4b5aa183af5c0f9712548fb
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-18 14:07:32 -0400

    Added sign in form for react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom'
 
 import logo from './logo.svg';
 import './App.css';
+import Signin from './components/Signin/Signin'
 
 class App extends Component {
-  
+  constructor(props) {
+    super();
+    this.state = {
+      jokes: []
+    }
+  }
 
-  // componentDidMount() {
-  //   axios.get(`http://localhost:5000/api/jokes`)
-  //   .then(res => {
-  //     this.setState({ jokes: res.data })
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //   })
-  // }
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/jokes`)
+    .then(res => {
+      console.log(res)
+      this.setState({ jokes: res.data })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
 
   render() {
     return (
@@ -26,11 +33,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-        <Route path = '/signin' component={Signin} />
-
+      <Route path = '/signin' component={Signin} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
